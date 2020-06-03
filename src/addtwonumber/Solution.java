217f2fbb3c5bf9732c547f3c28aeb30de51ae806
wujun36 <wujun36@mail2.sysu.edu.cn>
2020-06-03 18:54:00 +0800

    addTwoNumbers0603

--- src/addtwonumber/Solution.java
+++ src/addtwonumber/Solution.java
@@ -5,21 +5,40 @@ import org.omg.PortableServer.LIFESPAN_POLICY_ID;
 import java.util.List;
 
 public class Solution {
-    public static void main(String[] args){
-        ListNode l10 = new ListNode(2);
-        ListNode l11 = new ListNode(4);
-        ListNode l12 = new ListNode(3);
-        l10.next = l11;
-        l11.next = l12;
+    public static void main(String[] args) {
+        ListNode l10 = new ListNode(9);
+//        ListNode l11 = new ListNode(4);
+//        ListNode l12 = new ListNode(3);
+//        l10.next = l11;
+//        l11.next = l12;
 
-        ListNode l20 = new ListNode(5);
-        ListNode l21 = new ListNode(6);
-        ListNode l22 = new ListNode(4);
+        ListNode l20 = new ListNode(1);
+        ListNode l21 = new ListNode(9);
+        ListNode l22 = new ListNode(9);
+        ListNode l23 = new ListNode(9);
+        ListNode l24 = new ListNode(9);
+        ListNode l25 = new ListNode(9);
+        ListNode l26 = new ListNode(9);
+        ListNode l27 = new ListNode(9);
+        ListNode l28 = new ListNode(9);
+        ListNode l29 = new ListNode(9);
         l20.next = l21;
         l21.next = l22;
-
-        System.out.println(addTwoNumber(l10,l20).val);
+        l22.next = l23;
+        l23.next = l24;
+        l24.next = l25;
+        l25.next = l26;
+        l26.next = l27;
+        l27.next = l28;
+        l28.next = l29;
+        ListNode node = solution2(l10, l20);
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
     }
+
+    // TODO 会超出int的范围
     public static ListNode addTwoNumber(ListNode l1, ListNode l2) {
         ListNode ret = new ListNode(0);
         int count = 0;
@@ -46,5 +65,28 @@ public class Solution {
         }
 
         return ret0;
+    }
+
+    public static ListNode solution2(ListNode l1, ListNode l2) {
+        ListNode node = new ListNode(0);
+        ListNode ret = node;
+        int carry = 0;
+        while (!(l1 == null && l2 == null)) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            if (carry >= 10) node.val = carry % 10;
+            else node.val = carry;
+            carry /= 10;
+            node.next = new ListNode(carry);
+            node = node.next;
+        }
+        if (node.val == 0) node = null;
+        return ret;
     }
 }
