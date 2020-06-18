07fed576919d0c1212f40a123fcdc5f5dadb41c8
wujun36 <wujun36@mail2.sysu.edu.cn>
2020-06-18 17:21:28 +0800

    practice0618

--- src/lengthOfLongestSubstring/LengthOfLongestSubstring.java
+++ src/lengthOfLongestSubstring/LengthOfLongestSubstring.java
@@ -1,20 +1,41 @@
 package lengthOfLongestSubstring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
- *
+ * <p>
  * 示例：
  * 输入: "abcabcbb"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
  */
 public class LengthOfLongestSubstring {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
     }
 
-    public static int lengthOfLongestSubstring(String s){
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
 
-        return s.length();
+        int max = 0;
+        for (int i = 0; i < s.length() - max; i++) {
+            List<Character> list = new ArrayList<>();
+            for (int j = i; j < s.length(); j++) {
+                if (!list.contains(s.charAt(j))) {
+                    list.add(s.charAt(j));
+                } else {
+                    max = max > list.size() ? max : list.size();
+                    break;
+                }
+            }
+            if (list.size() == s.length() - i){
+                max = list.size();
+                break;
+            }
+        }
+        return max;
     }
 }
