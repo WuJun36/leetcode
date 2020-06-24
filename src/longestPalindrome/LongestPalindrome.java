6d1722e4abd996ab256631cf249cdf553d6090b4
wujun36 <wujun36@mail2.sysu.edu.cn>
2020-06-24 17:45:40 +0800

    practice0624

---
+++ src/longestPalindrome/LongestPalindrome.java
@@ -0,0 +1,20 @@
+package longestPalindrome;
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ */
+public class LongestPalindrome {
+
+    public static void main(String[] args){
+
+    }
+
+    public static String longestPalidrome(String s){
+
+        return s;
+    }
+}
